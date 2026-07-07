f0404446dae4b136dfee74f8618794b23043fa47
wijerett <winnettjeremy@gmail.com>
2026-07-07 05:31:44 -0400

    insert

--- bst.js
+++ bst.js
@@ -15,28 +15,20 @@ class Tree {
   }
 
   buildTree(array) {
-    //remove duplicates
     let uniqueArray = [...new Set(array)];
-    //sort
     uniqueArray = uniqueArray.sort((a, b) => a - b);
-    //call a recursive helper on the sorted array
-
     let root = this.#buildTreeRecursive(uniqueArray, 0, uniqueArray.length - 1 );
     return root;
   }
 
   #buildTreeRecursive(sortedArray, start, end) {
-    
     if (start > end) {
       return null;
     }
-    
     let mid = Math.floor((start + end) / 2);
-    
     let root = new Node(sortedArray[mid]);
     
     root.left = this.#buildTreeRecursive(sortedArray, start, mid - 1);
-    
     root.right = this.#buildTreeRecursive(sortedArray, mid + 1, end);
     
     return root;
@@ -56,6 +48,27 @@ class Tree {
     }
   }
 
+  insert(value, node = this.root) {
+    //accept and insert a new node with that value into the tree
+    const temp = new Node(value);
+    if (node === null) return temp;
+    //let current = node;
+    while (node !== null) {
+      if (node.data > value && node.left !== null) {
+        node = node.left;
+      } else if ( node.data < value && node.right !== null) {
+        node = node.right;
+      } else break;
+    }
+    if (node.data > value) node.left = temp;
+    else node.right = temp;
+    return node;
+    //1: make sure there are no matching values
+
+    //2: make sure nodes to the left are lower and to the right higher
+
+    //3: insert node
+  }
 
 
   prettyPrint(node = this.root, prefix = '', isLeft = true) {
@@ -76,5 +89,6 @@ let tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
 
 tree.prettyPrint();
-console.log(tree.includes(11));
+console.log(tree.includes(1500));
+console.log(tree.insert(99));
 
