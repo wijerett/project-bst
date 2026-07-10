b95de8dfd3f64302d4800d8c0f3afed154379370
wijerett <winnettjeremy@gmail.com>
2026-07-10 10:08:11 -0400

    tests

--- bst.test.js
+++ bst.test.js
@@ -2,6 +2,15 @@ import { Node } from "./bst.js";
 import { Tree } from "./bst.js";
 
 
-const node = Node();
-const tree = Tree();
+const node = new Node();
+let tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
+
+test('is present', () => {
+    expect(tree.includes(5)).toBe(true);
+});
+
+test('is inserted', () => {
+    tree.insert(99);
+    expect(tree.includes(99)).toBe(true);
+});
